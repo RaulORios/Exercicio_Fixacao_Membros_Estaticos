b8a7f2b0b5675f7163726fd039b491f0fe6b976e
RaulORios <raul.orios89@outlook.com>
2021-09-20 15:09:27 -0300

    Arquivos atualizados

---
+++ src/Exercicio_Fixacao_Membros_Estaticos/ConversorDeMoedas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Membros_Estaticos
+{
+    class ConversorDeMoedas
+    {
+        public double IOF = 6.0;
+        public double Cotacao;
+        public double Dollars;
+
+        public double Conversor()
+        {
+            return Dollars * Cotacao;
+        }
+
+        public double AjusteIOF()
+        {
+            return Conversor() * 0.06;
+        }
+
+        public double ValorFinal()
+        {
+            return Conversor() + AjusteIOF();
+        }
+
+    }
+}
---
+++ src/Exercicio_Fixacao_Membros_Estaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Membros_Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoedas c = new ConversorDeMoedas();
+
+            Console.Write("Qual é a cotação do dollar? ");
+            c.Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos Dólares você vai comprar? ");
+            c.Dollars = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            Console.WriteLine($"Valor a ser pago em Reais = {c.ValorFinal().ToString("F2",CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
